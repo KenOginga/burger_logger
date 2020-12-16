986d1b0dd54ae2d11740e665409f53bbb91da2ec
KenOginga <72943649+KenOginga@users.noreply.github.com>
2020-12-15 22:58:32 -0600

    initial push to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL){
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Miocene235@#%",
+    password: "your password",
     database: "burgers_db"
   });
 }
